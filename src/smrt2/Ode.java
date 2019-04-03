7cd3e19c14b1399adaf1dc17d2b095e746d929d2
Dennie te Molder <dennie.temolder@wur.nl>
2019-04-03 15:04:59 +0000

    DM,BW: Added state and formula variables and their getters and setters. All functions tested.
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@13 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/smrt2/Ode.java
+++ src/smrt2/Ode.java
@@ -1,5 +1,28 @@
 package smrt2;
 
 public class Ode {
+	private String formula;
+	private String state;
 	
+	public Ode(String state, String formula){
+		this.formula = formula;
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getFormula() {
+		return this.formula;
+	}
+
+	public void setFormula(String formula) {
+		this.formula = formula;
+		
+	}
 }
--- src/tests/testODE.java
+++ src/tests/testODE.java
@@ -4,8 +4,33 @@ import junit.framework.TestCase;
 import smrt2.Ode;
 
 public class testODE extends TestCase {
+	private String state = "E";
+	private String formula = "mc^2";
+	
 	public void testOdeExists(){
-		Ode myOde = new Ode();
+		Ode myOde = new Ode(null, null);
 		assertNotNull(myOde);
+	}
+	
+	public void testGetFormula(){
+		Ode myOde = new Ode(null, formula);
+		assertEquals(formula, myOde.getFormula());
+	}
+	
+	public void testSetFormula(){
+		Ode myOde = new Ode(null, null);
+		myOde.setFormula(formula);
+		assertEquals(formula, myOde.getFormula());
+	}
+	
+	public void testGetState(){
+		Ode myOde = new Ode(state, null);
+		assertEquals(state, myOde.getState());
+	}
+	
+	public void testSetState(){
+		Ode myOde = new Ode(null, null);
+		myOde.setState(state);
+		assertEquals(state, myOde.getState());
 	}
 }
