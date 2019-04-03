469b617eeb5d33d9c38a817a569d90fbec540152
Daniël Slingerland <daniel.slingerland@wur.nl>
2019-04-03 15:00:20 +0000

    SB DS: Added menu bar and tabbed pane
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@12 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/gui/SmartV2.java
+++ src/gui/SmartV2.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JToolBar;
+import java.awt.GridBagConstraints;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
 
 public class SmartV2 extends JFrame {
-
-	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,11 +41,51 @@ public class SmartV2 extends JFrame {
 	 */
 	public SmartV2() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setBounds(100, 100, 625, 486);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnModel = new JMenu("Model");
+		menuBar.add(mnModel);
+		
+		JMenuItem mntmNew = new JMenuItem("New...");
+		mnModel.add(mntmNew);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open...");
+		mnModel.add(mntmOpen);
+		
+		JMenuItem mntmSave = new JMenuItem("Save...");
+		mnModel.add(mntmSave);
+		
+		JMenu mnExperiment = new JMenu("Experiment");
+		menuBar.add(mnExperiment);
+		
+		JMenuItem mntmNew_1 = new JMenuItem("New...");
+		mnExperiment.add(mntmNew_1);
+		
+		JMenuItem mntmOpen_1 = new JMenuItem("Open...");
+		mnExperiment.add(mntmOpen_1);
+		
+		JMenuItem mntmSave_1 = new JMenuItem("Save...");
+		mnExperiment.add(mntmSave_1);
+		
+		JMenuItem mntmRun = new JMenuItem("Run...");
+		mnExperiment.add(mntmRun);
+		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		getContentPane().add(tabbedPane);
+		
+		table = new JTable();
+		tabbedPane.addTab("New tab", null, table, null);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		tabbedPane.addTab("New tab", null, lblNewLabel_1, null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		
+		
 	}
 
 }
