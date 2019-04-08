172340b850b06ff22489fdc95f0db82ee294fb22
Daniël Slingerland <daniel.slingerland@wur.nl>
2019-04-08 13:43:01 +0000

    TB DS: Added a parameter class to be used in the experiment.
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@54 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

---
+++ src/smrt2/Parameter.java
@@ -0,0 +1,19 @@
+package smrt2;
+
+public class Parameter {
+	private String name;
+	private double value;
+	public Parameter(String string, double d) {
+		this.name = string;
+		this.value = d;
+	}
+	
+	public double getValue() {
+		return this.value;
+	}
+
+	public void setValue(double d) {
+		this.value = d;
+	}
+
+}
---
+++ src/tests/ParametersTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import junit.framework.TestCase;
+import smrt2.Parameter;
+
+
+public class ParametersTest extends TestCase {
+	private Parameter p;
+	
+	public void setUp() {
+		p = new Parameter("p", 0.92);
+		assertNotNull(p);
+	}
+	
+	public void testGetValue() {
+		p = new Parameter("p", 0.92);
+		double got = p.getValue();
+		assertEquals(0.92, got);	
+	}
+	
+	public void testSetValue(){
+		p = new Parameter("p", 0.92);
+		p.setValue(0.93);
+		double got = p.getValue();
+		assertEquals(0.93, got);
+		
+		
+		
+	}
+}
