cee8d5c661ac32e97855a137849c4eaa22878d04
Shashi Baldoe <shashi.badloe@wur.nl>
2019-04-10 15:05:25 +0000

    BW SB: removed prompt
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@86 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/gui/FileChooser.java
+++ src/gui/FileChooser.java
@@ -6,6 +6,7 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileChooser {
+	String filePath;
 
 	// Opens a dialog to select a <type> to open, returns path of file.
 	public static String open(String type, String extension) {
