67754ff90d64948a17265bafa3cb3e8b4a092872
Daniel Prince <danielahprince@live.com>
2019-04-10 14:33:13 +0000

    DS DP: Created GeneralAlert class.
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@83 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

---
+++ src/gui/GeneralAlert.java
@@ -0,0 +1,48 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class GeneralAlert extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+
+	/**
+	 * Create the dialog.
+	 */
+	public GeneralAlert(String alertMessage) {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			JLabel lblAlertmessage = new JLabel(alertMessage);
+			contentPanel.add(lblAlertmessage);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
