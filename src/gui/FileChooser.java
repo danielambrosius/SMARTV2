cf3a354ea1ad886ab35ea69e337f6139137c2898
Shashi Baldoe <shashi.badloe@wur.nl>
2019-04-11 14:09:20 +0000

    DP SB: Finished implementing save functionality with overwrite checking.
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@103 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/gui/FileChooser.java
+++ src/gui/FileChooser.java
@@ -31,46 +31,49 @@ public class FileChooser {
 	}
 	
 	// Opens a dialog to select a <type> to open, returns path of file.
-		public static String save(String type, String extension) throws NullPointerException {
-		    boolean acceptable = false;
-		    String filePath = null;
-		    
-		    do {
-		        filePath = null;
-		        File f = null;
-		        String fn = null;
-		        
-		        JFileChooser jfc = new JFileChooser("./data/");
-		        jfc.setDialogTitle("Save the " + type);
-				jfc.setAcceptAllFileFilterUsed(false);
-				FileNameExtensionFilter filter = new FileNameExtensionFilter(type + " files", extension);
-				jfc.addChoosableFileFilter(filter);
-				
-		        if (jfc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
-		            filePath = jfc.getSelectedFile().getAbsolutePath();
-		            f = jfc.getSelectedFile();
-		            jfc.setSelectedFile(f);
-		            if (f.exists()) {
-		                int result = JOptionPane.showConfirmDialog(null, filePath.split("\\\\")[filePath.split("\\\\").length-1] + " exists, overwrite?",
-		                        "Existing file", JOptionPane.YES_NO_CANCEL_OPTION);
-		                if (result == JOptionPane.YES_OPTION) {
-		                    acceptable = true;
-		                }
-		            } else {
-		                acceptable = true;
-		            }
-		        } else {
-		        	acceptable = true;
-		        	return null;
-		        }
-		    } while (!acceptable);
-		    if (!filePath.endsWith(extension)) {
-		    	return filePath + "." + extension;
-		    }
-		    else {
-		    	return filePath;
-		    }
-		}
+	public static String save(String type, String extension) throws NullPointerException {
+	    boolean acceptable = false;
+	    String filePath = null;
+	    
+	    do {
+	        filePath = null;
+	        File f = null;
+	        
+	        JFileChooser jfc = new JFileChooser("./data/");
+	        jfc.setDialogTitle("Save the " + type);
+			jfc.setAcceptAllFileFilterUsed(false);
+			FileNameExtensionFilter filter = new FileNameExtensionFilter(type + " files", extension);
+			jfc.addChoosableFileFilter(filter);
+			
+	        if (jfc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+	            filePath = jfc.getSelectedFile().getAbsolutePath();
+	            if (filePath.endsWith(extension)) {
+	            	f = jfc.getSelectedFile();
+	            	
+	            }
+	            else {
+	            	f = new File(filePath + "." + extension);
+	            	filePath = f.getAbsolutePath();
+	            }
+	
+	            if (f.exists()) {
+	                int result = JOptionPane.showConfirmDialog(null, filePath.split("\\\\")[filePath.split("\\\\").length-1] + " exists, overwrite?",
+	                        "Existing file", JOptionPane.YES_NO_CANCEL_OPTION);
+	                if (result == JOptionPane.YES_OPTION) {
+	                    acceptable = true;
+	                }
+	            } else {
+	                acceptable = true;
+	            }
+	        } else {
+	        	acceptable = true;
+	        	return null;
+	        }
+	    } while (!acceptable);
+	    
+	    return filePath;
+	    
+	}
 	
 	
 	// Main method just for test purposes.
