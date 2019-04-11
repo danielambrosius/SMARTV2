87696bc26e84537a419be7504a9907429a6eeb20
Daniël Slingerland <daniel.slingerland@wur.nl>
2019-04-11 15:15:20 +0000

    DM DS: Solver works with a string that contains refrences to an array.
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@110 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/smrt2/Solver.java
+++ src/smrt2/Solver.java
@@ -7,7 +7,7 @@ import javax.script.ScriptException;
 public class Solver {
 	private String[] odeFormulas;
 	private double[] S0;
-	private double[] P;
+	private String P;
 	private double tStep;
 	private double nTimesteps; 
 	private double[][] S;
@@ -15,27 +15,48 @@ public class Solver {
 	public Solver(String[] odeFormulas, double[] S0, double[] P, double tEnd, double tStep) {
 		this.odeFormulas = odeFormulas;
 		this.S0 = S0;
-		this.P = P;
 		this.tStep = tStep;
 		this.nTimesteps = tEnd/tStep;
 		if(odeFormulas.length != S0.length ) {
 			throw new IllegalArgumentException("Number of odeFormulas and State variables do not match");			
 		}
-		this.S = new double[(int) nTimesteps+1][odeFormulas.length+1];		
+		this.S = new double[(int) nTimesteps+1][odeFormulas.length+1];
+		this.P = "";
+		
+		for (int i = 0; i < P.length; i++) {
+			if (i ==0) {
+				this.P += P[i];
+			}
+			else {
+				this.P += "," + P[i];
+			}
+		}
+		
+		
 	}
 	
-	private double[] eulerForeward(String[] odeFormulas ,double[] S, double[] P, double dt) {
+	private double[] eulerForeward(String[] odeFormulas ,double[] S, String P, double dt) {
 		ScriptEngineManager mgr = new ScriptEngineManager();
 		ScriptEngine engine = mgr.getEngineByName("JavaScript");
 		double[] results = new double[S.length];
 		double dxdt;
+		String states = "";
+		
+		for (int i = 0; i < S.length; i++) {
+			if (i == 0) {
+				states += S[i];
+			}
+			else {
+				states += "," + S[i];
+			}
+		}
 		
 		results[0] = S[0] + dt;
 		
 		for (int i = 1; i <= odeFormulas.length; i++) {
 			String odeFormula = odeFormulas[i-1];
 			try {
-				dxdt = Double.parseDouble(engine.eval(odeFormula).toString());
+				dxdt = Double.parseDouble(engine.eval(String.format("var P = [%s]; var S = [%s] ;%s", P, states, odeFormula )).toString());
 				results[i] = S[i] + dxdt*dt;
 			} catch (NumberFormatException e) {
 				e.printStackTrace();
