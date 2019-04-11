aa1c5d0fe5651089c6f4bba7bb99027d799a31e8
Bram van Wersh <bram.vanwersch@wur.nl>
2019-04-11 14:35:46 +0000

    BW TB: made the experiment window close on its own instead of all windows
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@107 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/gui/ExperimentView.java
+++ src/gui/ExperimentView.java
@@ -44,7 +44,7 @@ public class ExperimentView extends JFrame {
 	}
 
 	private void buildGui() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 575, 414);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
