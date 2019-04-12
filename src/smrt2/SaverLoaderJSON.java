c4c70f3477ac31cc07f477132ffb7446b3b09294
Daniël Slingerland <daniel.slingerland@wur.nl>
2019-04-12 15:28:43 +0000

    DP DS: Created a SaverLoader class
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@127 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

---
+++ src/smrt2/SaverLoaderJSON.java
@@ -0,0 +1,13 @@
+package smrt2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class SaverLoaderJSON {
+	private ObjectMapper mapper;
+	//TODO create a saver loader that can handle JSON
+}
