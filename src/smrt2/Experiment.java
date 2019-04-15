7645d7395e2a3d6503fd4d54a9587d050efe0d83
Dennie te Molder <dennie.temolder@wur.nl>
2019-04-15 13:34:19 +0000

    DM: added documentation to the non self explanatory parts of the experiment class & minor clean up.
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@137 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/smrt2/Experiment.java
+++ src/smrt2/Experiment.java
@@ -15,10 +15,14 @@ public class Experiment implements Serializable{
 	private double tStep;
 	
 	public Experiment(Model m) {
-		model = m;
-		setTimeFrame(0, 10, 1);
+		setTimeFrame(0, 10, 1); //default timeframe values
+		model = m; //store the passed on model inside the class
+		
+		//Create an array for initial conditions of states and fill it with default values
 		stateInitialValues = new Double[m.getStates().size()];
 		Arrays.fill(stateInitialValues, 1.0);
+		
+		//Create an array to store the parameter values, with default values
 		parameterValues = new Double[m.getParameters().size()];
 		Arrays.fill(parameterValues, 1.0);
 	}
@@ -43,7 +47,7 @@ public class Experiment implements Serializable{
 	}
 	
 	public String getName() {
-		
+		//TODO add ability to set the experiment name
 		return "Experiment";
 	}
 
@@ -67,6 +71,11 @@ public class Experiment implements Serializable{
 	}
 
 	private Map<String, String> buildParamDict() {
+		/**
+		 * Returns a HashMap that links a parameter name to a reference of an index in array P.
+		 * Array P will hold user specified parameter values.
+		 * This method is used by the reconstructFormula method.
+		 */
 		Map<String,String> paramDict = new HashMap<String,String>();
 		List<String> params = model.getParameters();
 		for (int i = 0; i < params.size();i++) {
@@ -77,6 +86,11 @@ public class Experiment implements Serializable{
 	}
 	
 	private Map<String, String> buildStatesDict(){
+		/**
+		 * Returns a HashMap that links a state name to a reference of an index in array S.
+		 * Array S contains the solutions of the model at the previous step in time.
+		 * This method is used by the reconstructFormula method.
+		 */
 		Map<String, String> statesDict = new HashMap<String, String>();
 		List<String> states = model.getStates();
 		for (int i = 0; i < states.size();i++) {
@@ -87,38 +101,49 @@ public class Experiment implements Serializable{
 	}
 
 	public String[] reconstructFormulas() {
-		int nStates = model.getStates().size();
+		/**
+		 * Returns a String[] containing user specified formulas in solver compatible syntax.
+		 * Parameters and states are replaced by references to arrays that will store the data.
+		 */
+		int nStates = model.getStates().size(); //number of states
+		//create a list to store the results
+		//each state has a corresponding formula so they have the same length
 		String[] reconstuctedFormulaList = new String[nStates];
+		//create a HashMap that translates variable names to array references that will hold the data.
 		Map<String,String> varDict = buildParamDict();
-		varDict.putAll(buildStatesDict());
+		varDict.putAll(buildStatesDict()); //combine both HashMaps for convenience.
 		
 		for (int i = 0; i < nStates; i++) {
 			String reconstructedFormula = "";
 			String[] currentVariables = model.getVariablesOfOde(i);
 			String[] currentOperators = model.getOperatorsOfOde(i);
 			
+			// the parser adds a empty string to either the list of variables or list of operators.
+			// the empty string ends up in the list of whatever the formula starts with.
+			// this information is used to reconstruct the formula in the correct order.
 			if (currentVariables[0].isEmpty()){
+				// the list that contains the empty string could be larger so index on the length of the other list 
 				for (int j = 0; j < currentOperators.length; j++) {
 					reconstructedFormula += varDict.get(currentVariables[j]) + currentOperators[j];
 				}
 			}
 			
-			else if (currentOperators.length > 0) {
-				if (currentOperators[0].isEmpty()){
-					for (int j = 0; j < currentVariables.length; j++) {
-						reconstructedFormula += currentOperators[j] + varDict.get(currentVariables[j]);
-					}
+			//check if ode equation has an operator
+			else if (currentOperators.length > 0 && currentOperators[0].isEmpty()) {
+				for (int j = 0; j < currentVariables.length; j++) {
+					reconstructedFormula += currentOperators[j] + varDict.get(currentVariables[j]);
 				}
 			}
-		
-			if (currentVariables.length < currentOperators.length){
-				reconstructedFormula += currentOperators[currentOperators.length-1];
-			}
 			
+			// if the variable list was larger that the operator list the last variable needs to be added and vice versa
 			if (currentVariables.length > currentOperators.length){
 				reconstructedFormula += varDict.get(currentVariables[currentVariables.length-1]);
 			}
 			
+			if (currentVariables.length < currentOperators.length){
+				reconstructedFormula += currentOperators[currentOperators.length-1];
+			}
+
 		reconstuctedFormulaList[i] = reconstructedFormula;
 		}
 		
@@ -126,12 +151,23 @@ public class Experiment implements Serializable{
 	}
 
 	public void setTimeFrame(double tStart, double tEnd, double tStep) {
+		/**
+		 * Used to set the time frame used by the solver. 
+		 * The solver always starts at t=0, but tStart is used by the gui to display a potential subset.
+		 * @param tStart used by gui to shows the desired time frame
+		 * @param tEND determines at what time point the solver should stop
+		 * @param tStep determines the step size the solver uses
+		 */
 		this.tStart = tStart;
 		this.tEnd = tEnd;
 		this.tStep = tStep;		
 	}
 
 	public Double[][] run() {
+		/**
+		 * Returns the solution of the experiment, with time in the first column.
+		 * Uses the solver class which employs the Euler foreward method.
+		 */
 		Solver s = new Solver(reconstructFormulas(), this.stateInitialValues, this.parameterValues, this.tEnd, this.tStep);
 		return s.solve();
 	}
