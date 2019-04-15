d31d0d88756ccf808bc5bfdc217933654beb7078
Daniel Prince <danielahprince@live.com>
2019-04-15 13:22:32 +0000

    BW DP: implemented JSON (re)constructor for Ode.class
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@135 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/smrt2/Ode.java
+++ src/smrt2/Ode.java
@@ -1,28 +1,44 @@
 package smrt2;
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import smrt2.EquationParser2;
 
 
 public class Ode implements Serializable{
 	// initialize variables
 	// State and formula should not contain trailing and leading white spaces.
-	private String equation;
+	private String formula;
 	private String state;
 	private String[] variables;
 	private String[] operators;
 	
 	// create constructor
 	public Ode(String state, String equation){
-		this.equation = equation;
+		this.formula = equation;
 		this.state = state;
 		
-		if (this.equation != null) {
+		if (this.formula != null) {
 			EquationParser2 parser = new EquationParser2(equation);
 			this.variables = parser.getVariables();
 			this.operators = parser.getOperators();
 		}
 	}
+	
+	// Constructor for JSONifier, this makes Jackson able to reconstruct the class.
+	@JsonCreator
+	public Ode(@JsonProperty("formula") String formula, 
+			   @JsonProperty("state") String state, 
+			   @JsonProperty("variables") String[] variables,
+			   @JsonProperty("operators") String[] operators) {
+		this.formula = formula;
+		this.state = state;
+		this.variables = variables;
+		this.operators = operators;
+	}
+	
 	// Getters and setters for variables
 	public String getState() {
 		return state;
@@ -33,18 +49,18 @@ public class Ode implements Serializable{
 	}
 
 	public String getFormula() {
-		return this.equation;
+		return this.formula;
 	}
 
 	public void setFormula(String equation) {
-		this.equation = equation;
+		this.formula = equation;
 		
 	}
 	
 	@Override
 	public String toString(){
 		String stringOde = "";
-		stringOde = "d" + this.state + "/dt = " + this.equation;
+		stringOde = "d" + this.state + "/dt = " + this.formula;
 		return stringOde;
 	}
 	public String[] getVariables() {
--- src/smrt2/SaverLoaderJSON.java
+++ src/smrt2/SaverLoaderJSON.java
@@ -1,5 +1,6 @@
 package smrt2;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -7,7 +8,39 @@ import java.io.ObjectInputStream;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
+
+
 public class SaverLoaderJSON {
+	
+	public static void main(String[] args) {
+		//Model myOde  = (Model) SaverLoader.load("./data/model_for_json.model");
+		
+		Ode myOde = new Ode("A", "hello some text");
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.writeValue(new File("./data/test_Json.json"), myOde);
+			String jsonString = mapper.writeValueAsString(myOde);
+			System.out.println(jsonString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+		
+		try {
+			Ode yourOde = mapper.readValue(new File("./data/test_Json.json"), Ode.class);
+			String jsonString = mapper.writeValueAsString(yourOde);
+			System.out.println(jsonString);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+	
+	
 	private ObjectMapper mapper;
 	//TODO create a saver loader that can handle JSON
 }
