2903df7756076e9c428c4ec8c20d1b6284ee6538
Daniël Slingerland <daniel.slingerland@wur.nl>
2019-04-15 14:18:12 +0000

    SB DS: Graphbuilder takes user data and creates graph.
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@139 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/gui/GraphBuilder.java
+++ src/gui/GraphBuilder.java
@@ -3,55 +3,60 @@ package gui;
 //https://www.developer.com/java/data/working-with-javafx-chart-apis.html
 //credits to: Manoj Debnath
 
-import java.util.*;
+
 
 import javafx.application.Application;
-import javafx.collections.*;
+
 import javafx.scene.Scene;
-import javafx.scene.chart.*;
-import javafx.scene.chart.XYChart.Series;
+
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.util.StringConverter;
 
-public class GraphBuilder{
 
-public static void main(String[] args) {
-   launch(args);
-}
 
-private static void launch(String[] args) {
-	createLineChart(int xNumber, Double[][] inputdata)
-}
 
-//@Override
-public static void start(Stage stage) throws Exception {
-   stage.setTitle("Chart");
-   StackPane pane = new StackPane();
-   pane.getChildren().add(createLineChart(1, null));
-   stage.setScene(new Scene(pane, 400, 200));
-   stage.show();
-}
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 
-public static Series<Double, Double> ArraysToSeries(int xNumber, Double[][] inputdata){
-	Series<Double, Double> chartdata = new Series<Double, Double>();
+
+public class GraphBuilder extends Application{
+
+
+public static XYChart.Series<Number, Number> ArraysToSeries(int xNumber, Double[][] inputdata){
+	XYChart.Series<Number, Number> chartdata = new XYChart.Series<Number, Number>();
 	for (int i=0; i < inputdata.length; i++) {
-		chartdata.getData().add(new XYChart.Data<Double,Double>(inputdata[i][xNumber], inputdata[i][0]));
+		chartdata.getData().add(new XYChart.Data<Number, Number>(inputdata[i][0], inputdata[i][xNumber]));
 	}
 	return chartdata;
 }
 
 
+public static void main(String[] args) {
+    launch(args);
+}
 
-public static XYChart<CategoryAxis, NumberAxis> createLineChart(int xNumber, Double[][] inputdata) {
-   LineChart lc = new LineChart<>(new NumberAxis(), new NumberAxis());   
-   Series<Double, Double> chartData = ArraysToSeries(xNumber, inputdata);
-   lc.getData().add(chartData);
-   lc.setTitle("Line chart on random data");
-   Stage stage = new Stage();
-   stage.setTitle("Second Chart");
-   stage.setScene(new Scene(lc, 500, 400));
-   stage.show();
-   return lc;
+@Override
+public void start(Stage primaryStage) {
+    NumberAxis xAxis = new NumberAxis();
+    NumberAxis yAxis = new NumberAxis();
+    final LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis);
+    //TEST
+    Double[][] testData = {{0.0, 0.0, 0.0}, {1.0, 1.0, 0.0}, {2.0, 2.0, 2.0}, {3.0, 3.0, 6.0}, {4.0, 4.0, 12.0}, {5.0, 5.0, 20.0}, {6.0, 6.0, 30.0},
+			{7.0, 7.0, 42.0}, {8.0, 8.0, 56.0}, {9.0, 9.0, 72.0}, {10.0, 10.0, 90.0}};
+
+    lineChart.setTitle("Test");
+    XYChart.Series<Number, Number> seriesData = ArraysToSeries(2, testData);
+    seriesData.setName("testSeries");
+    
+    Scene scene = new Scene(lineChart, 700, 800);
+    lineChart.getData().add(seriesData);
+    lineChart.setTitle("Chart");
+    primaryStage.setTitle("LineChart example");
+
+    StackPane root = new StackPane();
+    root.getChildren().add(lineChart);
+    primaryStage.setScene(new Scene(root, 400, 250));
+    primaryStage.show();
 }
 }
