c8231c542b1b0353b97a5ff38a88b41821779134
Tim Bosman <tim1.bosman@wur.nl>
2019-04-18 14:42:51 +0000

    TB DS: The solver test is now implemented for differing starting times and ending times.
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@186 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/tests/SolverTest.java
+++ src/tests/SolverTest.java
@@ -12,24 +12,36 @@ public class SolverTest extends TestCase {
 		Double[] P = {1., 2.};
 		double tEnd = 10;
 		double tStep = 1;
+		double tStart = 0.0;
 		
 		Double[][] expected = {{0.0, 0.0, 0.0}, {1.0, 1.0, 0.0}, {2.0, 2.0, 2.0}, {3.0, 3.0, 6.0}, {4.0, 4.0, 12.0}, {5.0, 5.0, 20.0}, {6.0, 6.0, 30.0},
 				{7.0, 7.0, 42.0}, {8.0, 8.0, 56.0}, {9.0, 9.0, 72.0}, {10.0, 10.0, 90.0}};
 		Solver s = new Solver();
-		Double[][] actual = s.solveEulerForward(Formulas, S0, P, tEnd, tStep);
+		Double[][] actual = s.solveEulerForward(Formulas, S0, P, tEnd, tStep, tStart);
 		
 		for (int i = 0; i < actual.length; i++) {
 			for (int j = 0; j < actual[i].length; j++) {
-//				System.out.printf("%5s = %5s | ", expected[i][j], actual[i][j]);
 				assertEquals(expected[i][j], actual[i][j]);
 			}
-//		System.out.println();
 		}
-		
-
-		
 	}
-	
-	
-	
+
+	public void testSolverCreationWithDifferentStartTime() {
+		String[] Formulas = {"P[0]", "P[1]*S[1]"};
+		Double[] S0 = {0., 0.};
+		Double[] P = {1., 2.};
+		double tEnd = 3;
+		double tStep = 1.0;
+		double tStart = 2.0;
+		
+		Double[][] expected = {{2.0, 0.0, 0.0}, {3.0, 1.0, 0.0}};
+		Solver s = new Solver();
+		Double[][] actual = s.solveEulerForward(Formulas, S0, P, tEnd, tStep, tStart);
+		
+		for (int i = 0; i < actual.length; i++) {
+			for (int j = 0; j < actual[i].length; j++) {
+				assertEquals(expected[i][j], actual[i][j]);
+			}
+		}	
+	}
 }
