67561287e2c7ac9c6a9a1d351289bd3cce3b6e4b
Daniël Slingerland <daniel.slingerland@wur.nl>
2019-04-23 12:39:38 +0000

    DS SB: fixed GraphBuilderTest
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@194 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/tests/GraphBuilderTest.java
+++ src/tests/GraphBuilderTest.java
@@ -6,10 +6,10 @@ import junit.framework.TestCase;
 
 public class GraphBuilderTest extends TestCase {
 	public void testArraysToSeries() {
-		Double[][] testInput = {{10.0, 120.0, 0.0}};
-		Series<Number,Number> seriesResult = GraphBuilder.ArraysToSeries(1, testInput);
-		String expected = "[Data[10.0,120.0,null]]";
-		System.out.println(seriesResult.getData().toString());
+		Double[] testInput = {10.0, 120.0, 0.0};
+		Double[] testTime = {1., 2., 3.};
+		Series<Number,Number> seriesResult = GraphBuilder.ArraysToSeries(testTime, testInput);
+		String expected = "[Data[1.0,10.0,null], Data[2.0,120.0,null], Data[3.0,0.0,null]]";
 		assertEquals(expected,seriesResult.getData().toString());
 	}
 
