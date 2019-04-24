9e05c490eacddd9030f2cf2ff86bb78bc74ed988
Tim Bosman <tim1.bosman@wur.nl>
2019-04-24 15:45:08 +0000

    TB DP: Last warning fixed before iteration 4 starts
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@221 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/smrt2/FileNotClosedException.java
+++ src/smrt2/FileNotClosedException.java
@@ -3,6 +3,10 @@ package smrt2;
 import java.io.IOException;
 
 public class FileNotClosedException extends IOException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String message;
 	
 	FileNotClosedException(String message) {
