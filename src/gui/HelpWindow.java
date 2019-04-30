c581b592bf8cec0c366147bba92ca22ea83ff6fa
Bram van Wersh <bram.vanwersch@wur.nl>
2019-04-30 15:22:30 +0000

    BW SB: added a HelpWindow gui and text file to display.
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@280 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

---
+++ src/gui/HelpWindow.java
@@ -0,0 +1,38 @@
+package gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+
+public class HelpWindow extends JFrame {
+
+	private JPanel contentPane;
+
+	public HelpWindow(String helpText) {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		JLabel textLabel = new JLabel("");
+		textLabel.setVerticalAlignment(SwingConstants.TOP);
+		textLabel.setOpaque(true);
+		textLabel.setBackground(Color.WHITE);
+		scrollPane.setViewportView(textLabel);
+		textLabel.setText(helpText);
+	}
+
+}
