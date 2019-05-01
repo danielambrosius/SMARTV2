a0d21631a1ad54d651ef179013c1220526574985
Shashi Baldoe <shashi.badloe@wur.nl>
2019-05-01 14:33:02 +0000

    DS SB: Resized defult TableViewer GUI size to properly show legend.
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@298 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/gui/TableViewer.java
+++ src/gui/TableViewer.java
@@ -52,7 +52,7 @@ public class TableViewer extends JFrame {
 		this.setTitle("SmartV2 - " + this.tableModel.getName());
 		
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 500);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
