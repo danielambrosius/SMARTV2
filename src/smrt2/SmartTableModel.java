598160f970aae7b688fe765d7d63fc2010e0df83
Bram van Wersh <bram.vanwersch@wur.nl>
2019-05-02 14:59:17 +0000

    BW TB: added docstrings to SmartTableModel
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@329 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/smrt2/SmartTableModel.java
+++ src/smrt2/SmartTableModel.java
@@ -21,10 +21,19 @@ public class SmartTableModel extends AbstractTableModel {
 		this.name = name;
 	}
 	
+	/**
+	 * Returns the name of the SmartTableModel.
+	 * @return: the names.
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * Returns data from a specified column
+	 * @param i: the index of the column.
+	 * @return: the data in that column.
+	 */
 	public Double[] getColumnAt(int i) {
 		Double[] columnData = new Double[getRowCount()];
 		for (int j = 0; j < getRowCount(); j++) {
@@ -33,10 +42,19 @@ public class SmartTableModel extends AbstractTableModel {
 		return columnData;
 	}
 	
+	/**
+	 * Returns data from a specified row
+	 * @param i: the index of the row.
+	 * @return: the data in that row.
+	 */
 	public Double[] getRowAt(int i){
 		return data.get(i);
 	}
 	
+	/**
+	 * Used to dynamically add data to the SmartTableModel
+	 * @param row: the row of data to be added.
+	 */
 	public void AddRow(Double[] row){
 		data.add(row);
 		fireTableRowsInserted(getRowCount(), getRowCount());
@@ -62,6 +80,10 @@ public class SmartTableModel extends AbstractTableModel {
 		return this.colNames.get(i);
 	}
 
+	/**
+	 * Used to get the names of the columns
+	 * @return: array of column names.
+	 */
 	public String[] getColumnNames() {
 		String[] newToReturn = new String[getColumnCount()];
 		for(int i = 0; i < getColumnCount(); i++) {
