00fc12beeea03ad3c8459650eda93c24affdd3d5
Tim Bosman <tim1.bosman@wur.nl>
2019-05-02 14:52:54 +0000

    TB BW: added docstrings to the saverloader
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@326 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/smrt2/SaverLoader.java
+++ src/smrt2/SaverLoader.java
@@ -17,28 +17,41 @@ public class SaverLoader {
 		mapper.enable(SerializationFeature.INDENT_OUTPUT);
 	}
 	
+	/**
+	 * Used to get have only one instance of this class
+	 * @return the instance of this class
+	 */
 	public static SaverLoader getInstance() {
 		if (instance == null) {
 			instance = new SaverLoader();
 		}
 		return instance;
 	}
-
+	
+	/**
+	* Used to save a file
+	* @param savePath is the path where to save the file 
+	* @param obj is the class of the saved file
+	*/
 	public void save(String savePath, Object obj) {
 		try {
 			mapper.writeValue(new File(savePath), obj);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Used to load a file
+	 * @param savePath is the path which file to load
+	 * @param cls is the class that has to be loaded
+	 * @return
+	 */
 	public Object load(String savePath, Class<?> cls) {
 		Object myClass = null;
 		 try {
 			myClass = mapper.readValue(new File(savePath), cls);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return myClass;
