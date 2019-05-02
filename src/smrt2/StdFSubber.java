c98b46d10c1e7bf7d0dc0c7f18ab5128b5695379
Bram van Wersh <bram.vanwersch@wur.nl>
2019-05-02 15:22:12 +0000

    BW TB: extracted a method in StdFSubber
    git-svn-id: https://harmoniqua.wur.nl/svn/group2019g/GroupG@333 a0d23bb5-bd9f-3140-b4fc-f659273aacb8

--- src/smrt2/StdFSubber.java
+++ src/smrt2/StdFSubber.java
@@ -21,6 +21,11 @@ public class StdFSubber {
 		for (int i = 0; i < subOperators.length; i++) {
 			odeOperator = odeOperator.replace(subOperators[i],targetOperators[i]);
 		}
+		odeOperator = replacingFunctions(odeOperator);
+		return odeOperator;
+	}
+
+	private static String replacingFunctions(String odeOperator) {
 		for (String function : standardFunctions) {
 			odeOperator = odeOperator.replace(function, "Math."+function);
 		}
